edb9c7f14abf0664fcb340ee578710f6e841968e
Nisreen AlBaik <nisreen.baik@gmail.com>
2019-12-05 11:57:30 +0200

    Validating and Saving Question

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -20,17 +20,21 @@
                             @csrf
                             <div class="form-group">
                                 <label for="question-title">Question Title</label>
-                                <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
+                                <input type="text" name="title" id="question-title" value="{{old('title')}}" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
 
                                 @if ($errors->has('title'))
                                     <div class="invalid-feedback">
                                         <strong>{{ $errors->first('title') }}</strong>
                                     </div>
                                 @endif
+
+
                             </div>
                             <div class="form-group">
                                 <label for="question-body">Explain you question</label>
-                                <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"></textarea>
+                                <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}">
+                                    {{old('body')}}
+                                </textarea>
 
                                 @if ($errors->has('body'))
                                     <div class="invalid-feedback">
