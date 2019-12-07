cf4f83f489206f43dbedd9daf1765c2fe795b7af
Nisreen AlBaik <nisreen.baik@gmail.com>
2019-12-07 20:45:58 +0200

    displaying-answers-for-question

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -10,7 +10,8 @@
                             <h1>{{$question->title}}</h1>
                             <div class="ml-auto">
                                 <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all
-                                    Questions</a>
+                                    Questions
+                                </a>
                             </div>
                         </div>
 
@@ -18,9 +19,53 @@
 
                     <div class="card-body">
                        {!! $question->body_html !!}
+                        <div class="float-right">
+                            <span class="text-muted">Answered {{ $question->created_date }}</span>
+                            <div class="media mt-2">
+                                <a href="{{ $question->user->url }}" class="pr-2">
+                                    <img src="{{ $question->user->avatar }}">
+                                </a>
+                                <div class="media-body mt-1">
+                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h2>{{ $question->answers_count . " " . str_plural('Answer', $question->answers_count) }}</h2>
+                        </div>
+                        <hr>
+                        @foreach ($question->answers as $answer)
+                            <div class="media">
+                                <div class="media-body">
+                                    {!! $answer->body_html !!}
+                                    <div class="float-right">
+                                        <span class="text-muted">Answered {{ $answer->created_date }}</span>
+                                        <div class="media mt-2">
+                                            <a href="{{ $answer->user->url }}" class="pr-2">
+                                                <img src="{{ $answer->user->avatar }}">
+                                            </a>
+                                            <div class="media-body mt-1">
+                                                <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 @endsection
