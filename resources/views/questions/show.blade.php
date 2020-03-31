c9088bb996383316a28c9588c610e8d1190f1ae9
Nisreen Albaik <nisreen.baik@gmail.com>
2020-03-31 18:57:28 +0300

    tidying

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -24,18 +24,6 @@
                         
                         <div class="media-body">
                             {!! $question->body_html !!}
-<<<<<<< HEAD
-							
-                            <div class="float-right">
-                                <span class="text-muted">Answered {{ $question->created_date }}</span>
-                                <div class="media mt-2">
-                                    <a href="{{ $question->user->url }}" class="pr-2">
-                                        <img src="{{ $question->user->avatar }}">
-                                    </a>
-                                    <div class="media-body mt-1">
-                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                    </div>
-=======
                             <div class="row">
                                 <div class="col-4"></div>
                                 <div class="col-4"></div>
@@ -44,7 +32,6 @@
                                         'model' => $question,
                                         'label' => 'asked'
                                     ])
->>>>>>> refactoring-views
                                 </div>
                             </div>
                         </div>
